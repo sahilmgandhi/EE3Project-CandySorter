bc104d7d2da34db1f907c47ad0959b97cdb07254
Sahil Gandhi <sahilmgandhi@gmail.com>
2016-05-11 00:31:37 -0700

    sweep update

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -12,7 +12,7 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 40;    // variable to store the servo position
+int pos;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -20,15 +20,28 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    Serial.println(pos);
-    delay(50);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(50);                       // waits 15ms for the servo to reach the position
-  }
+//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    Serial.println(pos);
+//    delay(50);                       // waits 15ms for the servo to reach the position
+//  }
+//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(50);                       // waits 15ms for the servo to reach the position
+//  }
+
+    int rand1 = random()%3;
+
+    Serial.println(rand1);
+    if (rand1 == 0)
+      myservo.write(2);
+    else if (rand1 == 1)
+      myservo.write(90);
+    else
+      myservo.write(179);
+
+    Serial.println(myservo.read());
+    delay(1000);
 }
 
